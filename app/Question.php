142e6ab63dfa9ac7b3375635e0ac1986b92ac50f
Reda El Henawy <reda.henawi7@gmail.com>
2019-06-12 14:41:24 +0200

    Design Answers table schema and rename answers column to answers_count

--- app/Question.php
+++ app/Question.php
@@ -31,7 +31,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if($this->answers > 0)
+        if($this->answers_count > 0)
         {
             if($this->best_answer_id) 
             { 
@@ -47,4 +47,11 @@ class Question extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+
+    }
+
 }
