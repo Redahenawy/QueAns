49dab35bf05f7a749211588e419a9fac9c81a18c
Reda El Henawy <reda.henawi7@gmail.com>
2019-07-22 15:15:14 +0200

    create accept answer component

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -7,11 +7,18 @@ use App\Answer;
 
 class AcceptAnswerController extends Controller
 {
-    public function __invoke(Answer $answer) 
+    public function __invoke(Request $request, Answer $answer) 
     {
     	//dd('Accept Answer');
     	$this->authorize('accept', $answer);
+
     	$answer->question->AcceptBestAnswer($answer);
+
+    	if (request()->expectsJson()) {
+    		return response()->json([
+    			'message' => "You have accepted this answer as best answer"
+    		]);
+    	}
     	return back();
     }
 }
